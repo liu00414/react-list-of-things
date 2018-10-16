6bba8bcbdd13ec1a88dd09713c8e373c445c198e
Le <liu00414@algonquinlive.com>
2018-10-15 22:22:20 -0400

    finished

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -11,7 +11,7 @@ class ListContainer extends Component {
       <div>
         <h3>List of Singers</h3>
         <ul>
-        {dataArray.map((obj,index)=>(<Item key={obj.id} id={obj.id} obj={obj} nameValue={obj.name} countryValue={obj.country} changeParentState={this.props.changeParentState} />))}
+        {dataArray.map((obj,index)=>(<Item key={obj.id} id={obj.id} nameValue={obj.name} countryValue={obj.country} changeParentState={this.props.changeParentState} />))}
         
         </ul>
         </div>
