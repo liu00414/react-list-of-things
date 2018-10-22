7f69265e81c922b7dfc3cf45fa8a0f38606d691e
Le <liu00414@algonquinlive.com>
2018-10-22 16:47:42 -0400

    Updates

---
+++ precache-manifest.b07acce4c0760cdc2106683e7060b981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-list-of-things/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7a05aac199b99e1e6c93",
+    "url": "/react-list-of-things/static/js/runtime~main.7a05aac1.js"
+  },
+  {
+    "revision": "1c8e5aef4c671403dda4",
+    "url": "/react-list-of-things/static/js/main.1c8e5aef.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/react-list-of-things/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "1c8e5aef4c671403dda4",
+    "url": "/react-list-of-things/static/css/main.9a2c87cf.chunk.css"
+  },
+  {
+    "revision": "99d035976b7e8a36d925ad6c72f79636",
+    "url": "/react-list-of-things/index.html"
+  }
+];
